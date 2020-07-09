64a96bcebd6c1b82d25b81307d16d7d63093c1ad
Szymon Borowy <szymonborowy@wp.pl>
2020-07-09 23:36:00 +0200

    Update Room model

--- src/main/java/pl/sborowy/hotelroomsbookingapp/model/Room.java
+++ src/main/java/pl/sborowy/hotelroomsbookingapp/model/Room.java
@@ -1,15 +1,26 @@
 package pl.sborowy.hotelroomsbookingapp.model;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@EqualsAndHashCode(of = "id")
 public class Room {
 
+    // --fields--
     private int id;
     private String number;
     private String numberOfPeople;
     private double costPerDay;
-    private boolean isAvailable = true;
+    private boolean isAvailable;
+
+    // --constructors--
+    public Room(String number, String numberOfPeople, double costPerDay) {
+        this.number = number;
+        this.numberOfPeople = numberOfPeople;
+        this.costPerDay = costPerDay;
+        this.isAvailable = true;
+    }
 }
