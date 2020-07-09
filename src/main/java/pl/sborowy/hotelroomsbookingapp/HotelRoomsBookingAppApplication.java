ba19f162126deed022b28abae9758dd94b72addf
Szymon Borowy <szymonborowy@wp.pl>
2020-07-09 20:38:10 +0200

    Init project

---
+++ src/main/java/pl/sborowy/hotelroomsbookingapp/HotelRoomsBookingAppApplication.java
@@ -0,0 +1,12 @@
+package pl.sborowy.hotelroomsbookingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelRoomsBookingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelRoomsBookingAppApplication.class, args);
+	}
+}
